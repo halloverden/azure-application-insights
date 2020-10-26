e917d5e6a4eb32e11997dc474e9fce0d3ac0c8c6
Marco <marco@braains.no>
2020-10-26 14:37:34 +0100

    FIX: license

--- Tests/Bootstrap.php
+++ Tests/Bootstrap.php
@@ -1,3 +1,12 @@
 <?php
 
+/*
+ * This file is part of the microsoft/application-insights package.
+ *
+ * (c) Microsoft Corporation
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 require_once 'vendor/autoload.php';
--- Tests/TestUtils.php
+++ Tests/TestUtils.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of the microsoft/application-insights package.
+ *
+ * (c) Microsoft Corporation
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace HalloVerden\ApplicationInsights\Tests;
 
 use HalloVerden\ApplicationInsights\Channel\Contracts\Application;
